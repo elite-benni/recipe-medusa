2c007ab1ea008bff26a2dd383f9f012f6e709b8a
elite-benni <benjamin.heiss@elite.tirol>
2024-11-26 07:00:16 +0000

    add cors header for test to custom route

--- src/api/store/custom/route.ts
+++ src/api/store/custom/route.ts
@@ -1,8 +1,6 @@
 import { MedusaRequest, MedusaResponse } from "@medusajs/framework";
 
-export async function GET(
-  req: MedusaRequest,
-  res: MedusaResponse
-): Promise<void> {
+export async function GET(req: MedusaRequest, res: MedusaResponse): Promise<void> {
+  res.appendHeader("Access-Control-Allow-Origin", "*");
   res.sendStatus(200);
 }
